f8da653b161ef9d4612b5ef344297cf1f0d06bc8
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-01 22:15:51 +0800

    0701 從localStorage改成session但抓不到資料

--- src/pages/Cart/components/ScContent1/ScItem.js
+++ src/pages/Cart/components/ScContent1/ScItem.js
@@ -3,134 +3,72 @@ import ScPriceRow from './ScPriceRow'
 
 function ScItem(props) {
   const [mycart, setMycart] = useState([])
-  // const [dataLoading, setDataLoading] = useState(false)
-  const [mycartDisplay, setMycartDisplay] = useState([])
 
-  function getCartFromLocalStorage() {
-    // 開啟載入的指示圖示
-    // setDataLoading(true)
+  async function getProductsFromServer(productId){
+    // 連接的伺服器資料網址
+    const url = 'http://localhost:5500/cart/product'
 
-    const newCart = localStorage.getItem('cart') || '[]'
+    // 注意header資料格式要設定，伺服器才知道是json格式
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
 
-    console.log(JSON.parse(newCart))
-    setMycart(JSON.parse(newCart))
+    const response = await fetch(request)
+    const data = await response.json()
+    console.log(data)
+    // 設定資料
+    setMycart(data)
   }
+
+  
+  // 一開始就會開始載入資料
   useEffect(() => {
-    getCartFromLocalStorage()
+    getProductsFromServer()
+    // console.log(mycart)
   }, [])
 
-  // componentDidUpdate
-  useEffect(() => {
-    // setTimeout(() => setDataLoading(false), 1000)
-
-    // mycartDisplay運算
-    let newMycartDisplay = []
-
-    //尋找mycartDisplay
-    for (let i = 0; i < mycart.length; i++) {
-      //尋找mycartDisplay中有沒有此mycart[i].id
-      const index = newMycartDisplay.findIndex(
-        (value) => value.id === mycart[i].id
-      )
-      //有的話就數量+1
-      if (index !== -1) {
-        newMycartDisplay[index].amount += mycart[i].amount
-      } else {
-        //沒有的話就把項目加入，數量為1
-        const newItem = { ...mycart[i] }
-        newMycartDisplay = [...newMycartDisplay, newItem]
-      }
-    }
-
-    console.log(newMycartDisplay)
-    setMycartDisplay(newMycartDisplay)
-  }, [mycart])
-
-  // 更新購物車中的商品數量
-  const updateCartToLocalStorage = (item, isAdded = true) => {
-    console.log(item, isAdded)
-    const currentCart = JSON.parse(localStorage.getItem('cart')) || []
-
-    // find if the product in the localstorage with its id
-    const index = currentCart.findIndex((v) => v.id === item.id)
-
-    console.log('index', index)
-    // found: index! == -1
-    if (index > -1) {
-      isAdded ? currentCart[index].amount++ : currentCart[index].amount--
-    }
-
-    localStorage.setItem('cart', JSON.stringify(currentCart))
-
-    // 設定資料
-    setMycart(currentCart)
-  }
-
-  // 參考ProductList.js中updateCartToLocalStorage概念
-  // 製作按下X按鈕執行delItem函式刪除localStorage單筆資料
-  const delItem = (item) => {
-    // 先複製原有的購物車內容
-    const currentCart = JSON.parse(localStorage.getItem('cart')) || []
-
-    // 找尋是否有此筆item.id的對應資料
-    const index = currentCart.findIndex((v) => v.id === item.id)
-    
-    if (index > -1) {
-      // 找到的話就透過splice來移除array中的那個物件
-      // 再更新至localStorage cart之中並且更新Mycart
-      currentCart.splice(index, 1)
-      localStorage.setItem('cart', JSON.stringify(currentCart))
-      setMycart(currentCart)
-    }
-  }
-  
-
-  // const loading = (
-  //   <>
-  //     <div className="d-flex justify-content-center">
-  //       <div className="spinner-border" role="status">
-  //         <span className="sr-only">Loading...</span>
-  //       </div>
-  //     </div>
-  //   </>
-  // )
 
   const display = (
     //   為何最外層還是要加上<></>
     // 商品列
       <>
-      {mycartDisplay.map((item, index) => {
+      {mycart.map((item, index) => {
         return (
-        <div key={item.id} className="itemRow d-flex bdBottom align-items-center position-relative">
+        <div key={item.itemId} className="itemRow d-flex bdBottom align-items-center position-relative">
             <div className="col-6 d-flex align-items-center">
               <div className="myCheckbox selectOne ml-4" />
               <div className="itemPic ml-5" />
               <div className="sc-nameFont itemName">
-                <div className="mb-0">{item.name}</div>
+                <div className="mb-0">{item.itemName}</div>
               </div>
             </div>
             <div className="d-flex col-2 justify-content-center">
               <div
                 className="sc-qtyFont p-2 scBtn scBtnSub"
-                onClick={() => {
-                  if (item.amount === 1) return
-                  updateCartToLocalStorage(item, false)
-                }}
+                // onClick={() => {
+                //   if (item.amount === 1) return
+                //   updateCartToLocalStorage(item, false)
+                // }}
               >
                 -
               </div>
-              <div className="sc-qtyFont p-2">{item.amount}</div>
+              <div className="sc-qtyFont p-2">{item.quantity}</div>
               <div
                 className="sc-qtyFont p-2 scBtn scBtnAdd"
-                onClick={() => updateCartToLocalStorage(item, true)}
+                // onClick={() => updateCartToLocalStorage(item, true)}
               >
                 +
               </div>
             </div>
-            <div className="sc-priceFont col-2">{item.price}</div>
-            <div className="sc-priceFont col-2">{item.amount * item.price}</div>
+            <div className="sc-priceFont col-2">{item.itemPrice}</div>
+            <div className="sc-priceFont col-2">{item.quantity * item.itemPrice}</div>
             <div className="delOne position-absolute"
-                  onClick={()=>{delItem(item)}}>
+                  // onClick={()=>{delItem(item)}}
+                  >
               <i className="fas fa-times p-3 scBtn " />
             </div>
         </div>
@@ -145,7 +83,7 @@ function ScItem(props) {
     {/* {dataLoading ? loading : display} */}
     { display}
     <ScPriceRow
-      mycartDisplay={mycartDisplay} />
+      mycart={mycart} />
     </>
   )
 }
--- src/pages/Cart/components/ScContent1/ScPriceRow.js
+++ src/pages/Cart/components/ScContent1/ScPriceRow.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 
 function ScPriceRow(props) {
-  const { mycartDisplay } = props
+  const { mycart } = props
 
   // 計算總價用的函式
   const sum = (items) => {
     let total = 0
     for (let i = 0; i < items.length; i++) {
-      total += items[i].amount * items[i].price
+      total += items[i].quantity * items[i].itemPrice
     }
     return total
   }
 
    // 計算總商品數量的函式
-   const amountSum = (items) => {
-    let totalAmount = 0
+   const qtySum = (items) => {
+    let totalQty = 0
     for (let i = 0; i < items.length; i++) {
-      totalAmount += items[i].amount
+      totalQty += items[i].quantity
     }
-    return totalAmount
+    return totalQty
   }
 
   return (
@@ -28,12 +28,12 @@ function ScPriceRow(props) {
       <div className="w-100 priceRow px-0">
         <div className=" col-10 bdBottom d-flex flex-column align-items-center py-5 mx-auto">
           <div className="w-100 totalQtyFont my-2 px-0 py-3 bdBottom">
-            共<span>{amountSum(mycartDisplay)}</span>件商品
+            共<span>{qtySum(mycart)}</span>件商品
           </div>
           <div className="w-100 d-flex jus justify-content-end my-2 px-0">
             <div className="totalPriceFont col-3 px-0">總計</div>
             <div className="totalPriceFont-med col-3 px-0">
-              NT<span>{sum(mycartDisplay)}</span>
+              NT<span>{sum(mycart)}</span>
             </div>
           </div>
         </div>

1d5e03014edac1abe5928a2a03163f167dc75007
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-02 11:30:27 +0800

    0702 改回localStorage版本

--- src/pages/Cart/components/ScContent1/ScPriceRow.js
+++ src/pages/Cart/components/ScContent1/ScPriceRow.js
@@ -2,24 +2,24 @@ import React from 'react'
 
 
 function ScPriceRow(props) {
-  const { mycart } = props
+  const { mycartDisplay } = props
 
   // 計算總價用的函式
   const sum = (items) => {
     let total = 0
     for (let i = 0; i < items.length; i++) {
-      total += items[i].quantity * items[i].itemPrice
+      total += items[i].amount * items[i].price
     }
     return total
   }
 
    // 計算總商品數量的函式
-   const qtySum = (items) => {
-    let totalQty = 0
+   const amountSum = (items) => {
+    let totalAmount = 0
     for (let i = 0; i < items.length; i++) {
-      totalQty += items[i].quantity
+      totalAmount += items[i].amount
     }
-    return totalQty
+    return totalAmount
   }
 
   return (
@@ -28,12 +28,12 @@ function ScPriceRow(props) {
       <div className="w-100 priceRow px-0">
         <div className=" col-10 bdBottom d-flex flex-column align-items-center py-5 mx-auto">
           <div className="w-100 totalQtyFont my-2 px-0 py-3 bdBottom">
-            共<span>{qtySum(mycart)}</span>件商品
+            共<span>{amountSum(mycartDisplay)}</span>件商品
           </div>
           <div className="w-100 d-flex jus justify-content-end my-2 px-0">
             <div className="totalPriceFont col-3 px-0">總計</div>
             <div className="totalPriceFont-med col-3 px-0">
-              NT<span>{sum(mycart)}</span>
+              NT<span>{sum(mycartDisplay)}</span>
             </div>
           </div>
         </div>

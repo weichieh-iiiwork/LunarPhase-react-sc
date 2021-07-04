e669bdc6753ef88b00ea2bbb4afd8e3bd83961e7
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-04 15:29:25 +0800

    0704 模擬加入訂單功能移置ScCartChecked檔案

--- src/pages/Cart/components/ScCartChecked/index.js
+++ src/pages/Cart/components/ScCartChecked/index.js
@@ -39,12 +39,67 @@ function ScCartChecked(props) {
   const toggleIsHidden = () => {
     setIsHidden(!isHidden)
   }
+
+  // `id`, `orderId`, `orderItemsId`, `checkPrice`, `checkQty`, `checkSubtotal`, `created_at`, `updated_at`
+  async function addOrderToSever(e) {
+    // e.preventDefault()
+    const orderId = +new Date()
+    let data = {
+      orderItems: []
+    }
+     for (let item of orderItemsStr) {
+       const tempObj = {
+        orderId: orderId,
+        orderItemsId: item.id,
+        checkPrice: item.price,
+        checkQty: item.amount,
+        checkSubtotal: item.price*item.amount,
+       }
+       data.orderItems.push(tempObj)
+     }
+    //  `orderId`, `username`, `orderPrice`, `paymentTypeId`, `created_at`, `updated_at`
+     data.orderInfo = {
+      orderId: orderId,
+      username: 'jessica',
+      orderPrice: sum(orderItemsStr),
+      paymentTypeId: 5,
+     }
+
+  
+    // 連接的伺服器資料網址
+    const url = 'http://localhost:5500/cart/product/order/add'
+  
+    // 注意資料格式要設定，伺服器才知道是json格式
+    // 轉成json檔傳到伺服器
+    const request = new Request(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
+    console.log('JSON',JSON.stringify(data))
+    // console.log('JSON parse',JSON.parse(JSON.stringify(data)).orderItems)
+  
+    const response = await fetch(request)
+    const dataRes = await response.json()
+  
+    console.log('伺服器回傳的json資料', dataRes)
+  
+  }
+
+
   return (
     <>
+      
       <div className="container-fluid">
         <div className="col-10 mx-auto px-0 shadow-sm ">
           {/*  bar  */}
           <div className="d-flex select-bar align-items-center"></div>
+          <button
+            onClick={()=>{addOrderToSever()}}
+            >模擬加入訂單</button>
           <div className="sc-Wrap d-flex flex-column justify-content-around">
             {showContent4 ? <ScOrderId/> : "" }        
             <ScItemChecked

bc8e81d163e4459e7e2cc1a3bd72696a3ab84764
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-05 16:39:06 +0800

    0705 刪除SelCon合併到上層

---
+++ src/pages/Cart/CartItem.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react'
+
+function CartItem() {
+
+  return(
+    <>
+      
+    </>
+  )
+}
+
+export default CartItem
--- src/pages/Cart/components/ScCartChecked/ScContent3/index.js
+++ src/pages/Cart/components/ScCartChecked/ScContent3/index.js
@@ -1,41 +1,78 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 // import ConData from './ConData'
 import HomeData from './HomeData'
 import SelPayment from './SelPayment'
 import CreditData from './CreditData'
-import SelCon from './SelCon'
+// import SelCon from './SelCon'
+import sevenCity from '../../../../../data/711/711.json'
+// 711.json
+// { "xx縣":[{store1},{store2}],"XX市":[{store1},{store2}] }
 
 function ScContent3(props) {
   const {
     isCon,
     paymentWay, setPaymentWay,
-    // conUserName, setConUserName,
-    // conUserPhone, setConUserPhone,
     homeUserName, setHomeUserName,
     homeUserPhone, setHomeUserPhone,
     homeUserAddress, setHomeUserAddress,
-
-
   } = props
-  // 第三頁會新增顯示出來的區塊
-  // const [paymentWay, setPaymentWay] = useState("")
-  // // // 想要刪除這個區塊，合併到另一個區塊之中
-  // // // const [conUserName, setConUserName] = useState('')
-  // // // const [conUserPhone, setConUserPhone] = useState('')
-  // const [homeUserName, setHomeUserName] = useState('')
-  // const [homeUserPhone, setHomeUserPhone] = useState('')
-  // const [homeUserAddress, setHomeUserAddress] = useState('')
+
   const [cardNum, setCardNum] = useState('')
   const [cardName, setCardName] = useState('')
   const [cardDate, setCardDate] = useState('')
   const [cardCode, setCardCode] = useState('')
+  const [seletedConCity, setSeletedConCity] = useState('')
+  const [seletedConStore, setSeletedConStore] = useState('')
 
+  let sevenStores = sevenCity[seletedConCity];
+  // useEffect(() => {
+  //   console.log('seletedConCity',seletedConCity)
+  //     console.log('sevenCity',sevenCity)
+  //     console.log('sevenStores',sevenStores)
+  //   }, [sevenStores])
+
+  const SelCon = (
+    <>
+      {/* 選擇取貨超商 */}
+      <div className="pb-2 mb-3 mt-5 titleDivider">
+        <div className="scTitle col-5">選擇取貨超商</div>
+      </div>
+      <div className="d-flex justify-content-start">
+        <h5 className="mx-2">711</h5>
+        <h5 className="mx-2">全家</h5>
+      </div>
+      <select className="scSelect sc-contentFont ml-5 my-3 w-25" name="conCity" 
+        value={seletedConCity}
+        onChange={(e) => {
+            setSeletedConCity(e.target.value)
+              }}>
+        <option value="-1">選擇縣市</option>
+        {/* key直接用city是因為只要unique就好 */}
+        { Object.keys(sevenCity).map(city => {
+            return(<option key={city} value={city}>{city}</option>)
+        })}
+
+      </select>
+      <select className="scSelect sc-contentFont ml-5 my-3 w-25" name="conStore" 
+        value={seletedConStore}
+        onChange={(e) => {
+            setSeletedConStore(e.target.value)
+              }}>
+        <option value="-1">選擇門市</option>
+        { seletedConCity &&
+          sevenStores.map((store,index)=>{
+            return(<option key={index} value={store.POIName}>{store.POIName}店</option>) 
+        })}
+      </select>
+    </>
+
+  )
 
 
 
   return(
     <>
-      <SelCon/>
+      {SelCon}
       <SelPayment
         paymentWay={paymentWay}
         setPaymentWay={setPaymentWay}

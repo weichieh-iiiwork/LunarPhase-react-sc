bc0a4ef33cc4b026208f227ef2744f5dbbbdc6cc
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-10 21:15:57 +0800

    0710 新增訂單查詢頁面Orderlist

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -1,50 +1,54 @@
-// import React, { useState, useEffect } from 'react'
-// import LunarPhaseHeader from '../components/LunarPhaseHeader'
+import React, { useState, useEffect } from 'react'
+import LunarPhaseHeader from '../components/LunarPhaseHeader'
 
-// function OrderList() {
- 
-//     async function getOrderListFromServer(){
-//         const url = 'http://localhost:5500/orderlist'
+function OrderList() {
+  const [order, setOrder] = useState([]) //初始資料
 
-//         // 注意資料格式要設定，伺服器才知道是json格式
-//         const request = new Request(url, {
-//             method: 'GET',
-//             headers: new Headers({
-//             Accept: 'application/json',
-//             'Content-Type': 'application/json',
-//             }),
-//         })
+  async function getOrderListFromServer() {
+    const url = 'http://localhost:5500/orderlist'
 
-//         const response = await fetch(request)
-//         const data = await response.json()
+    // 注意資料格式要設定，伺服器才知道是json格式
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
 
-//     }
-    
-//     useEffect(() => {
-//         getOrderListFromServer()
-//       }, [])
+    const response = await fetch(request)
+    const data = await response.json()
 
+    setOrder(data.data)
+  }
 
-//   return (
-//     <>
-//     <LunarPhaseHeader/>
-//     <div className="col-10 mt-5 mx-auto">
-//       <h2>訂單查詢</h2>
-//       <p></p>
-//       <div className="my-2 d-flex align-items-center py-3">
-//         <div className="col-5 d-flex align-items-center sc-nameFont">
-//           <div className="itemPic-sm mr-3" />
-//           <div>名稱</div>
-//         </div>
-//         <div className="col-1 sc-priceFont">數量</div>
-//         <div className="col-2 sc-priceFont">$ 價格</div>
-//         <div className="col-2 sc-priceFont">$ 小計</div>
-//         <div className="col-2 sc-priceFont">商品詳細頁</div>
-//       </div>
+  useEffect(() => {
+    getOrderListFromServer()
+  }, [])
 
-//     </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <LunarPhaseHeader />
+      <div className="col-10 mt-5 mx-auto">
+        <h2>訂單查詢</h2>
+        {order.map((v, i) => {
+          return (
+            <div className="my-2 d-flex align-items-center py-3">
+              <div className="col-5 d-flex align-items-center sc-nameFont">
+                <div className="itemPic-sm mr-3" />
+                <div>訂單號{v.orderId}</div>
+              </div>
+              <div className="col-1 sc-priceFont">訂購人{v.username}</div>
+              <div className="col-1 sc-priceFont">金額$ {v.orderPrice}</div>
+              <div className="col-1 sc-priceFont">運費$ {v.shippingPrice}</div>
+              <div className="col-1 sc-priceFont">付款方式 {v.paymentType}</div>
+              <div className="col-1 sc-priceFont">訂購時間 {v.created_at}</div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
 
-// export default OrderList
+export default OrderList

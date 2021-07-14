0a54f3d648e0606cd77f3adbacff302185cb5143
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-14 14:34:26 +0800

    0714 測試廣告列輪播

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../components/NavBar'
+import AdTest from './ScCarousel/AdTest'
+import ScAd from './Cart/components/ScAD'
 function Login() {
   const [account, setAccount] = useState('')
   const [password, setPassword] = useState('')
@@ -84,6 +86,11 @@ function Login() {
           登入
         </button>
       </div>
+      <div className="mt-5"></div>
+
+      <AdTest/>
+      <div className="mt-5"></div>
+      <ScAd/>
     </>
   )
 }
